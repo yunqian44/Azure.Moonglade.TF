a83b6fdcd69dd0889a62ad9f9ebea0859fe9de7d
yunqian44 <yunqian8@live.com>
2021-04-12 17:54:55 +0800

    add azure app service common module

---
+++ src/module/web_app/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_app_service_plan" "app_service_plan" {
+  count               = var.enable && var.enable_app_service_plan && var.app_service_plan_count > 0 ? var.app_service_plan_count : 0
+  name                = element(var.app_service_plan_names, count.index)
+  location            = element(var.app_service_locations, count.index)
+  resource_group_name = var.resource_group_name
+
+  sku {
+    tier = lookup(element(var.app_service_plans, count.index), "tier")
+    size = lookup(element(var.app_service_plans, count.index), "size")
+  }
+}
+
+resource "azurerm_app_service" "app_service" {
+  count               = var.enable && var.enable_app_service_plan && var.enable_app_service && var.app_service_count > 0 ? var.app_service_count : 0
+  name                = element(var.app_service_names, count.index)
+  location            = element(var.app_service_locations, count.index)
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = element(azurerm_app_servicle_plan.app_service_plan.*.id, count.index)
+
+  app_settings = element(var.app_settings, count.index)
+}
---
+++ src/module/web_app/variables.tf
@@ -0,0 +1,60 @@
+variable "enable" {
+  type        = bool
+  default     = false
+  description = "(required) main switch whether to create the app web service"
+}
+
+variable "enable_app_service_plan" {
+  type        = bool
+  default     = false
+  description = "(required) whether to create the app web service plan"
+}
+
+variable "app_service_plan_count" {
+  type        = number
+  default     = 0
+  description = "(required) number of create the app web service plan"
+}
+
+variable "enable_app_service" {
+  type        = bool
+  default     = false
+  description = "(required) whether to create the app web service"
+}
+
+variable "app_service_count" {
+  type        = number
+  default     = 0
+  description = "(required) number of create the app web service"
+}
+
+variable "app_service_plan_names" {
+  type        = list(string)
+  description = "(required) name of create a web app plan"
+}
+
+variable "app_service_names" {
+  type        = list(string)
+  description = "(required) specifies the name of the app service."
+}
+
+variable "app_service_locations" {
+  type        = list(string)
+  description = "The Azure Regions * where the Resource Group should exist. Changing this forces a new Resource Group to be created."
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = ""
+  description = "The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
+}
+
+variable "app_service_plans" {
+  description = "A map of sku to apply to web app plan"
+  type        = list(map(string))
+}
+
+variable "app_settings" {
+  description = "A list(map(string)) of app_settings to apply to web app"
+  type        = list(map(string))
+}
