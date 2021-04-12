984c0e0944d5cc7928f85f55b5d3f8ff31a88efa
yunqian44 <yunqian8@live.com>
2021-04-12 17:54:09 +0800

    add azure resource group common module

---
+++ src/module/resource_group/variables.tf
@@ -0,0 +1,20 @@
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to be applied to resources (inclusive)"
+  type        = map(string)
+}
+
+variable "names" {
+  description = "Names to be applied to resources (inclusive)"
+  type        = object({
+                  environment         = string
+                  location            = string
+                  market              = string
+                  product_name        = string
+                  resource_group_type = string
+                })
+}
