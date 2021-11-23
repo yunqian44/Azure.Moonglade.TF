d54a2b5ddf4c8598a45d313d92fad653f6adc3f2
yunqian44 <yunqian8@live.com>
2021-11-23 22:25:40 +0800

    modify web app module params

--- src/module/web_app/main.tf
+++ src/module/web_app/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_app_service_plan" "service_plan" {
   count               = var.enable && var.enable_app_service_plan && var.app_service_plan_count > 0 ? var.app_service_plan_count : 0
   name                = element(var.app_service_plan_names, count.index)
   location            = element(var.app_service_locations, count.index)
@@ -10,12 +10,11 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   }
 }
 
-resource "azurerm_app_service" "app_service" {
+resource "azurerm_app_service" "web_service" {
   count               = var.enable && var.enable_app_service_plan && var.enable_app_service && var.app_service_count > 0 ? var.app_service_count : 0
   name                = element(var.app_service_names, count.index)
   location            = element(var.app_service_locations, count.index)
   resource_group_name = var.resource_group_name
-  app_service_plan_id = element(azurerm_app_servicle_plan.app_service_plan.*.id, count.index)
-
-  app_settings = element(var.app_settings, count.index)
+  app_service_plan_id = element(azurerm_app_service_plan.service_plan.*.id, count.index)
+  app_settings        = element(var.app_settings, count.index)
 }
