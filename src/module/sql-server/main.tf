1fafcfe0f35c4fd70d01352ae4776611ac388d3a
yunqian44 <yunqian8@live.com>
2021-11-29 09:05:41 +0000

    delete unnecessary module

--- src/module/sql-server/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_sql_server" "sql_server" {
-  name                         = var.sql_server_name
-  resource_group_name          = var.resource_group_name
-  location                     = var.location
-  version                      = var.version
-  administrator_login          = var.sql_server_administrator_login
-  administrator_login_password = var.sql_server_administrator_login_password
-}
