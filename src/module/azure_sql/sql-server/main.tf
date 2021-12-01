88ac099ba684d2200ae1bb3cc3441e7c6407ea06
yunqian44 <yunqian8@live.com>
2021-12-01 08:59:33 +0000

    modify sql server module

--- src/module/azure_sql/sql-server/main.tf
+++ src/module/azure_sql/sql-server/main.tf
@@ -1,8 +1,9 @@
 resource "azurerm_sql_server" "sql_server" {
-  name                         = var.sql_server_name
+  count                        = var.enable_sql_server && var.sql_server_count > 0 ? var.sql_server_count : 0
+  name                         = element(var.sql_server_names, count.index)
   resource_group_name          = var.resource_group_name
   location                     = var.location
-  version                      = "12.0"
-  administrator_login          = var.sql_server_administrator_login
-  administrator_login_password = var.sql_server_administrator_login_password
+  version                      = element(var.sql_server_versions, count.index) //"12.0"
+  administrator_login          = element(var.sql_server_administrator_logins, count.index)
+  administrator_login_password = element(var.sql_server_administrator_login_passwords, count.index)
 }
--- src/module/azure_sql/sql-server/variables.tf
+++ src/module/azure_sql/sql-server/variables.tf
@@ -1,20 +1,40 @@
+variable "enable_sql_server" {
+  type        = bool
+  default     = false
+  description = "(required) main switch whether to create the SQL Server."
+}
+
+variable "sql_server_count" {
+  type        = number
+  default     = 0
+  description = "(required) number of create the SQL Server."
+}
+
 variable "resource_group_name" {
-  description="The name of the resource group in which to create the SQL Server."
+  description = "The name of the resource group in which to create the SQL Server."
 }
 
 variable "location" {
-  description="Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
+  description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
 }
 
-variable "sql_server_name" {
-  description="The name of the SQL Server. This needs to be globally unique within Azure."
+variable "sql_server_names" {
+  type        = list(string)
+  description = "The name of the SQL Server. This needs to be globally unique within Azure."
 }
 
-variable "sql_server_administrator_login" {
-  description="The administrator login name for the new server. Changing this forces a new resource to be created."
+variable "sql_server_versions" {
+  type        = list(string)
+  description = "(Required) The version for the new server. Valid values are: 2.0 (for v11 server) and 12.0 (for v12 server)."
 }
 
-variable "sql_server_administrator_login_password" {
-  description="The password associated with the administrator_login user. Needs to comply with Azure's Password Policy(https://msdn.microsoft.com/library/ms161959.aspx)"
+variable "sql_server_administrator_logins" {
+  type        = list(string)
+  description = "The administrator login name for the new server. Changing this forces a new resource to be created."
+}
+
+variable "sql_server_administrator_login_passwords" {
+  type        = list(string)
+  description = "The password associated with the administrator_login user. Needs to comply with Azure's Password Policy(https://msdn.microsoft.com/library/ms161959.aspx)"
 }
 
