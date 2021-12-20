1821424c18eb9ec5ca27e9ad01f33a91a3652846
yunqian44 <yunqian8@live.com>
2021-12-20 14:13:40 +0000

    1,output storage connection string

--- src/module/storage_account/outputs.tf
+++ src/module/storage_account/outputs.tf
@@ -9,3 +9,7 @@ output "storage_account_names" {
 output "storageAccountBlobUrl" {
   value = formatlist("%s.blob.core.windows.net", azurerm_storage_account.storageAccount.*.name)
 }
+
+output "connection_string" {
+  value = azurerm_storage_account.storageAccount.*.primary_connection_string
+}
